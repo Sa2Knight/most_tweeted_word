3659a7049a49b3fd9194a7ae0a1796c4f1674913
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-09 15:51:46 +0900

    関数名を変更

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # テキストを与えると、単語とその品詞のハッシュを返却する
-# do_mecab("私は歩く") => {"私" => "名詞" , "は" => "助詞" , "歩く" => "動詞"} 
-def do_mecab(text)
+# text_to_types("私は歩く") => {"私" => "名詞" , "は" => "助詞" , "歩く" => "動詞"} 
+def text_to_types(text)
   words = {}
   result = `echo #{text} | mecab -E "" | awk -F, '{print $1}'`
   result.each_line do |word|
