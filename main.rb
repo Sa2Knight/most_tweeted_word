770502c1a856cb9ee838a27aea9c3c54234dbf88
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-09 16:12:58 +0900

    テキストを与えると単語ごとの出現回数をカウントする関数を実装

--- main.rb
+++ main.rb
@@ -8,6 +8,17 @@ def text_to_words(text)
   return words
 end
 
+# テキストを与えると、単語ごとの出現回数をカウントする
+def text_to_words_count(text)
+  counts = Hash.new {|h , k| h[k] = 0}
+  text.lines do |line|
+    text_to_words(line.chomp).each do |w|
+      counts[w] += 1
+    end
+  end
+  return counts
+end
+
 # テキストを与えると、単語とその品詞のハッシュを返却する
 # text_to_types("私は歩く") => {"私" => "名詞" , "は" => "助詞" , "歩く" => "動詞"} 
 def text_to_types(text)
