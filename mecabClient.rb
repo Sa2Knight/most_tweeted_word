076050f52b1e34f92993557def9762d17ee4adaa
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-09 18:01:23 +0900

    単語取得時に、品詞を指定して取得できるように変更

--- mecabClient.rb
+++ mecabClient.rb
@@ -5,25 +5,27 @@ class MecabClient
     @text = text
   end
 
-  # 単語ごとに分割した配列を返却する
-  def to_words
-    words = []
-    result = `echo #{@text} | mecab -E "" | awk -F'\t' '{print $1}'`
-    result.lines do |line|
-      words.push line.chomp
+  # テキストを与えると、単語とその品詞のハッシュを返却する
+  def to_types
+    words = {}
+    result = `echo #{@text} | mecab -E "" | awk -F, '{print $1,$2}'`
+    result.each_line do |word|
+      sp = word.chomp.split(/\s/)
+      words[sp[0]] = "#{sp[1]} #{sp[2]}"
     end
     return words
   end
 
-  # テキストを与えると、単語とその品詞のハッシュを返却する
-  def to_types
-    words = {}
-    result = `echo #{@text} | mecab -E "" | awk -F, '{print $1}'`
-    result.each_line do |word|
-      sp = word.chomp.split(/\s/)
-      words[sp[0]] = sp[1]
+  # 単語ごとに分割した配列を返却する
+  # typeを指定すると、品詞を指定して取得できる
+  def to_words(type = nil)
+    words = []
+    words_with_type = to_types
+    if type
+      words_with_type.select {|k , v| v.match(type)}.keys
+    else
+      words_with_type.keys
     end
-    return words
   end
 
 end
