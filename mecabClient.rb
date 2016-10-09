80c221904026a4de3481f9f13d2f2f75602921bc
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-09 19:38:52 +0900

    シェルのエラーが出ないように修正

--- mecabClient.rb
+++ mecabClient.rb
@@ -8,7 +8,7 @@ class MecabClient
   # テキストを与えると、単語とその品詞のハッシュを返却する
   def to_types
     words = {}
-    result = `echo #{@text} | mecab -E "" | awk -F, '{print $1,$2}'`
+    result = `echo "#{@text}" | mecab -E "" | awk -F, '{print $1,$2}'`
     result.each_line do |word|
       sp = word.chomp.split(/\s/)
       words[sp[0]] = "#{sp[1]} #{sp[2]}"
